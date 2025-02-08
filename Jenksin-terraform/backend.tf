0ae9c6b9b12760b39ba77f013863fe7a38d28dec
Paras Pahwa <134779342+paraspahwa@users.noreply.github.com>
2025-02-08 21:33:57 +0530

    Update backend.tf

--- Jenksin-terraform/backend.tf
+++ Jenksin-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraformbucketdemo57" # Replace with your actual S3 bucket name
+    bucket = "terraformdemobucket" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
