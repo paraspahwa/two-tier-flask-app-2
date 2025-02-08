bf6dbb6802de4fb35094a4fad8b984e795fe87dd
Paras Pahwa <134779342+paraspahwa@users.noreply.github.com>
2025-02-08 20:17:46 +0530

    Update main.tf

--- Jenksin-terraform/main.tf
+++ Jenksin-terraform/main.tf
@@ -59,9 +59,9 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0522ab6e1ddcc7055"
+  ami                    = "ami-04b4f1a9cf54c11d0"
   instance_type          = "t2.large"
-  key_name               = "Mumbai-mac"
+  key_name               = "testing"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./script.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
