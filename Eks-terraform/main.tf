0687692821c92e3cb027cee2a39acd855d8128b5
Paras Pahwa <134779342+paraspahwa@users.noreply.github.com>
2025-02-08 20:46:23 +0530

    Update main.tf

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -38,7 +38,7 @@ resource "aws_eks_cluster" "example" {
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
-    subnet_ids = subnet_ids = [
+    subnet_ids = [
       aws_subnet.subnet_a.id,
       aws_subnet.subnet_b.id
     ]
